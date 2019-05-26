8d63a7333493ee569197d19a516eb0c5cacb79be
Franz <Franz@yahoo.com>
2019-05-27 04:52:03 +0800

    Uploaded a folder for Adding & Viewing Students for the Instructor

---
+++ Instructor Module/student-list/connect-mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PSWD', '');
+DEFINE ('DB_HOST', '');
+DEFINE ('DB_NAME', 'additem');
+
+$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+
+?>

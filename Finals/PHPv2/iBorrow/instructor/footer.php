5df44a9ebc93269075ee00aa3b447291afbbd582
haudrygen <2153221@slu.edu.ph>
2019-06-26 01:22:21 +0800

    created instructor partition

---
+++ Finals/PHPv2/iBorrow/instructor/footer.php
@@ -0,0 +1,17 @@
+<footer>
+            <div class="pull-right">
+                iBorrow
+            </div>
+            <div class="clearfix"></div>
+        </footer>
+    </div>
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/fastclick.js"></script>
+<script src="js/nprogress.js"></script>
+
+<script src="js/custom.min.js"></script>
+</body>
+</html>

2ffbc7491244b73142631901e8dbf632ac8a2d72
haudrygen <2153221@slu.edu.ph>
2019-07-06 00:14:32 +0800

    updated add_items table

--- Finals/PHPv2/database/add_items.sql
+++ Finals/PHPv2/database/add_items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2019 at 04:19 PM
+-- Generation Time: Jul 05, 2019 at 06:12 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.26
 
@@ -29,12 +29,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `add_items` (
-  `id` int(20) NOT NULL,
+  `id` int(5) NOT NULL,
   `item_name` varchar(20) NOT NULL,
   `item_image` varchar(500) NOT NULL,
-  `item_qty` varchar(20) NOT NULL,
-  `available_qty` int(20) NOT NULL
+  `item_qty` varchar(255) NOT NULL,
+  `available_qty` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `add_items`
+--
+
+INSERT INTO `add_items` (`id`, `item_name`, `item_image`, `item_qty`, `available_qty`) VALUES
+(1, 'shovel', 'items_image3140d771d1c098f587c65fb8530602d4gloves.jpg', '15', '15'),
+(2, 'gloves', 'items_imageac8ab83fbcd623170730c327dac22b0dgloves.jpg', '10', '10'),
+(3, 'rag', 'items_image187811e56aa3a85ac323a248a33bfcc7rag.jpg', '15', '15'),
+(4, 'broomstick', 'items_image7ac1be254b45cd012b3faf289ed4c508broomstick.jpg', '10', '10'),
+(5, 'dustpan', 'items_image202e3001a55ae51ee9c291755c635aa2dustpan.jpg', '10', '10');
 
 --
 -- Indexes for dumped tables
@@ -45,6 +56,16 @@ CREATE TABLE `add_items` (
 --
 ALTER TABLE `add_items`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `add_items`
+--
+ALTER TABLE `add_items`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
